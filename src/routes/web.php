ec390f29bba76c4c4e752ab4fcb54382ca20ec2b
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-01-14 14:29:05 +0000

    #2 Update to routes and composer

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use MattYeend\QueueMonitor\Http\Controllers\QueueMonitor;
+use MattYeend\QueueMonitor\Http\Controllers\QueueMonitorController;
+
+Route::middleware('web')->group(function() {
+    Route::get(config('queue-monitor.dashboard_route'), [QueueMonitorController::class, 'index']);
+});
